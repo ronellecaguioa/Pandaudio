bd2b8cd9b7236e11dd93c4f4fbba206b1dc57b3a
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-07-21 23:56:53 -0700

    Attempt at creating mock sockets

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,12 @@
 module.exports = {
   collectCoverage: true,
   rootDir: './',
-  testRegex: '__tests__/.+\\.test\\.js',
   transform: {
-    '^.+\\.js?$': 'babel-jest',
+    '^.+\\.jsx?$': 'babel-jest',
   },
-  moduleFileExtensions: ['js'],
   moduleDirectories: ['node_modules', 'lib'],
+  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(js|jsx)$',
+  moduleFileExtensions: ['js', 'json', 'jsx'],
+  // setupFiles: ['<rootDir>/__tests__/setup/socketMock.js'],
+  // setupFilesAfterEnv: ['./__tests__/setup/windowMock.js'],
 };

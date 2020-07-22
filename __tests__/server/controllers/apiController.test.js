4db2e1b1ba5bb92747544408bead99761c0440d8
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-07-21 23:11:07 -0700

    Create tests for server directory

---
+++ __tests__/server/controllers/apiController.test.js
@@ -0,0 +1,7 @@
+const apiController = require('../../../server/controllers/apiController');
+
+describe('server.js', () => {
+  it('should have a basic test', () => {
+    expect(1).toEqual(1);
+  });
+});
---
+++ __tests__/server/server.test.js
@@ -0,0 +1,7 @@
+const server = require('../../server/server');
+
+describe('server.js', () => {
+  it('should have a basic test', () => {
+    expect(1).toEqual(1);
+  });
+});

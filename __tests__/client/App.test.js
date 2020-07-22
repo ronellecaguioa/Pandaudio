a854e589c2708aeb3f6657bdb5c30736f7ae2fc7
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-07-21 23:57:06 -0700

    Create tests for components

--- __tests__/client/App.test.js
+++ __tests__/client/App.test.js
@@ -1,5 +1,12 @@
 // const App = require('../../client/App/index.jsx');
 
+// jest.mock('socket.io-client', () => {
+//   const emit = jest.fn();
+//   const on = jest.fn();
+//   const socket = { emit, on };
+//   return jest.fn(() => socket);
+// });
+
 describe('<App />', () => {
   it('should have a decent test', () => {
     expect('a string').toEqual('a string');
--- __tests__/client/components/LoginPage.test.js
+++ __tests__/client/components/LoginPage.test.js
@@ -1,1 +1,7 @@
 // import LoginPage from '../../../client/components/LoginPage/index';
+
+describe('<LoginPage />', () => {
+  it('should have a decent test', () => {
+    expect('a string').toEqual('a string');
+  });
+});

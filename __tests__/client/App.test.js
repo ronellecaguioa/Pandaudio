02821b0f170e06a48fe3a2e6028c2a1a83b81f29
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-07-21 23:12:03 -0700

    Create tests for the client directory

---
+++ __tests__/client/App.test.js
@@ -0,0 +1,7 @@
+// const App = require('../../client/App/index.jsx');
+
+describe('<App />', () => {
+  it('should have a decent test', () => {
+    expect('a string').toEqual('a string');
+  });
+});
---
+++ __tests__/client/components/LoginPage.test.js
@@ -0,0 +1,1 @@
+// import LoginPage from '../../../client/components/LoginPage/index';
